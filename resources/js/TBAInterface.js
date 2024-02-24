28e29371c3895646278fee216d142693bcbfcf39
Danica Stout <danicastout11@gmail.com>
2024-02-23 20:09:51 -0500

    added tba

--- resources/js/TBAInterface.js
+++ resources/js/TBAInterface.js
@@ -1,7 +1,7 @@
 // TBAInterface funcitons to pull data from TheBlueAlliance.com
 var teams = null;
 var schedule = null;
-var authKey = "uTHeEfPigDp9huQCpLNkWK7FBQIb01Qrzvt4MAjh9z2WQDkrsvNE77ch6bOPvPb6";
+var authKey = "3yCoyOnOjraNEEcX63QwG3p16V4kTZfFsu5Jqyq8gxkWykrY7eCxBKZe1SQ879dE";
 /**
  * Get list of teams in event
  *
